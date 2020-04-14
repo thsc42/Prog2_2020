c0f8451c53ceed2b24a632795ba5725146494e40
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-04-14 15:13:34 +0200

    interface added

---
+++ src/transmission/DataConnector.java
@@ -0,0 +1,18 @@
+package transmission;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class DataConnector implements DataConnection {
+
+    @Override
+    public DataInputStream getDataInputStream() throws IOException {
+        return null; // TODO
+    }
+
+    @Override
+    public DataOutputStream getDataOutputStream() throws IOException {
+        return null; // TODO
+    }
+}
